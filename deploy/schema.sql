9012a5d412f046c2071d79a51bc1bbd93c0da907
k33nice <k33nice@gmail.com>
2014-11-28 22:34:46 +0200

    test bash

--- deploy/schema.sql
+++ deploy/schema.sql
@@ -10,10 +10,10 @@ CREATE TABLE IF NOT EXISTS films(
 CREATE TABLE IF NOT EXISTS actors(
   id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор',
   name VARCHAR(200) NOT NULL COMMENT 'Имя актера',
-  surname VARCHAR(1024) COMMENT 'Фамилия актера',
+  surname VARCHAR(200) COMMENT 'Фамилия актера',
   film_id INT(11) UNSIGNED NOT NULL COMMENT 'Уникальный идентификатор фильма',
   PRIMARY KEY (id),
-  UNIQUE KEY (name, film_id),
+  UNIQUE KEY (name, surname, film_id),
   FOREIGN KEY (film_id)
   REFERENCES films.films(id)
     ON DELETE CASCADE
